f5e4f99065582887f33af3370aeae62aa332ef47
Aarush_009 <aarushkumarchoubey@gmail.com>
2022-09-05 16:23:23 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
         <FaCoins className="icon" />
         <h1>
           {" "}
-          Coin <span className="purple">Search</span>
+          Coin <span className="purple">search</span>
         </h1>
       </div>
     </Link>
